2cf033db5732c119052408bed9d87bcafd34f5bc
Clément MOINE <clement.moine86@gmail.com>
2025-01-08 15:54:01 +0100

    Fix: Remove unused useCallback

--- frontend/src/utils/mergeRefs.ts
+++ frontend/src/utils/mergeRefs.ts
@@ -1,13 +1,18 @@
-import { MutableRefObject, Ref, useCallback } from 'react';
+import { MutableRefObject, Ref } from 'react';
 
+/**
+ * Merges multiple refs into a single ref callback.
+ * @param refs - Refs to be merged.
+ * @returns A callback ref that updates all provided refs.
+ */
 export function mergeRefs<T>(...refs: (Ref<T> | undefined)[]): Ref<T> {
   return (value: T | null) => {
     refs.forEach(ref => {
-      if (!ref) return;
+      if (!ref) return; // Skip if ref is undefined
       if (typeof ref === 'function') {
-        ref(value);
+        ref(value); // Call the ref function with the value
       } else if ('current' in ref) {
-        (ref as MutableRefObject<T | null>).current = value;
+        (ref as MutableRefObject<T | null>).current = value; // Update the current property of the ref object
       }
     });
   };
